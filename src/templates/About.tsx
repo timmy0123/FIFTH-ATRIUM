f4eda50bcabf1f71160319a63072b5201a01ae42
鄭宇伸 <timmyabc10@gmail.com>
2023-02-17 12:54:24 +0800

    About content finish

--- src/templates/About.tsx
+++ src/templates/About.tsx
@@ -13,8 +13,8 @@ export const About: React.FC = () => (
             <Stack spacing={40} sx={{width: "98%"}}>
                 <Box sx={{ fontSize: 30}}>ANIMATION AND DESIGN STUDIO</Box>
                 <Stack spacing={5}>
-                <Box sx={{ fontSize: 25, width: "45%"}}>Through our artful animation and distinct design, we will infuse our dedicated passion for storytelling to align with your unique vision every step of the way.</Box>
-                <Box sx={{ fontSize: 25, width: "45%"}}>Offering a full-service suite that spans original graphics, logos, animation, videos, web design, and brand consulting, we are a studio that produces content for you and most importantly, with you.</Box>
+                <Box sx={{ fontSize: 20, width: "45%"}}>Through our artful animation and distinct design, we will infuse our dedicated passion for storytelling to align with your unique vision every step of the way.</Box>
+                <Box sx={{ fontSize: 20, width: "45%"}}>Offering a full-service suite that spans original graphics, logos, animation, videos, web design, and brand consulting, we are a studio that produces content for you and most importantly, with you.</Box>
                 </Stack>
             </Stack>
             <Box>FIFTH ATRIUM</Box>
@@ -31,15 +31,9 @@ export const About: React.FC = () => (
             <Box></Box>
         </Stack>
 
-        <Stack sx={{ flexDirection: {md: "row"}}}>
-            <Box> So</Box>
-            <Stack sx={{ flexDirection: {md: "row"}}}>
-                <Image src={im17} alt="" width="370" height="500"/>
-                <Image src={im18} alt="" width="370" height="500"/>
-                <Image src={im19} alt="" width="370" height="500"/>
-                <Image src={im20} alt="" width="370" height="500"/>
-            </Stack>
-            <Box></Box>
-        </Stack>
+        <Box sx={{ fontSize: 35, width: "45%"}}> So</Box>
+        <Box display="flex" justifyContent="right"> 
+            <Box sx={{ fontSize: 25, width:"30%"}}>Come plant your ideas with Fifth Atrium and see them spring to life!</Box>
+        </Box>
     </Stack>
 );
---
+++ src/templates/Contact.tsx
@@ -0,0 +1,3 @@
+import * as React from 'react';
+import { Box, Stack } from "@mui/material";
+import Divider from '@mui/material/Divider';
